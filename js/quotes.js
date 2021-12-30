912cc5cb6a32e4326856c471d175597e43c33e35
Hyunju37 <88761410+Hyunju37@users.noreply.github.com>
2021-12-30 23:13:43 +0900

    some CSS

--- js/quotes.js
+++ js/quotes.js
@@ -1,43 +1,43 @@
 const quotes = [
   {
     quote: "Be yourself; everyone else is already taken.",
-    author: "Oscar Wilde",
+    author: "  -Oscar Wilde",
   },
   {
     quote: "You only live once, but if you do it right, once is enough.",
-    author: "Mae West",
+    author: "  -Mae West",
   },
   {
     quote:
       "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
-    author: "Maya Angelou",
+    author: "  -Maya Angelou",
   },
   {
     quote:
       "Life is like riding a bicycle. To keep your balance, you must keep moving.",
-    author: "Albert Einstein",
+    author: "  -Albert Einstein",
   },
   {
     quote:
-      "Yesterday is history, tomorrow is a mystery, todat is a gift of God. Which is why we call it the present.",
-    author: "Bill Keane",
+      "Yesterday is history, tomorrow is a mystery, today is a gift of God. Which is why we call it the present.",
+    author: "  -Bill Keane",
   },
   {
     quote: "Be the change that you wish to see in the world.",
-    author: "Mahatma Gandhi",
+    author: "  -Mahatma Gandhi",
   },
   {
     quote: "It is never too late to be what you might have been.",
-    author: "George Eliot",
+    author: "  -George Eliot",
   },
   {
     quote: "마음의 문을 여는 손잡이는 바깥쪽이 아닌 안쪽에 있다.",
-    author: "헤겔",
+    author: "  -헤겔",
   },
   {
     quote:
       "우리는 자신을 앞으로 할 수 있는 것으로 판단하지만, 다른 사람은 우리를 우리가 이미 한 것으로 판단한다.",
-    author: "헨리 워즈워스 롱펠로",
+    author: "  -헨리 워즈워스 롱펠로",
   },
 ];
 const quote = document.querySelector("#quote span:first-child");
